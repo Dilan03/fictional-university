eb379e7885d103767866e7df92107de71ddccd3e
Dilan Garcia <dilanngcia@gmail.com>
2022-07-18 12:52:40 -0600

    loading and displaying professor info

--- wp-content/plugins/featured-professor/build/index.asset.php
+++ wp-content/plugins/featured-professor/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => '7b93f9eaf584151e2ccc');
+<?php return array('dependencies' => array('wp-data', 'wp-element'), 'version' => '514acf4f81e08ef6a6ab');
--- wp-content/plugins/featured-professor/featured-professor.php
+++ wp-content/plugins/featured-professor/featured-professor.php
@@ -8,6 +8,8 @@
 
 if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
+require_once plugin_dir_path(__FILE__).'inc/generateProfessorHTML.php';
+
 class FeaturedProfessor {
   function __construct() {
     add_action('init', [$this, 'onInit']);
@@ -25,7 +27,12 @@ class FeaturedProfessor {
   }
 
   function renderCallback($attributes) {
-    return '<p>We will replace this content soon.</p>';
+    if ($attributes['profId']) {
+      wp_enqueue_style('featuredProfessorStyle');
+      return generateProfessorHTML($attributes['profId']);
+    } else {
+      return NULL;
+    }
   }
 
 }
